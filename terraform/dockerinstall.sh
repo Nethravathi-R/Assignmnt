be5e9c61bb2f2c11b5ff47e477101350c45ab5cd
NethravathiR <154945584+Nethravathi-R@users.noreply.github.com>
2024-03-09 19:21:46 +0530

    Create dockerinstall.sh

---
+++ terraform/dockerinstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update
+
+sudo apt install -y docker-ce
+
+sudo systemctl status docker
+
+sudo usermod -aG docker $USER
+
+su - $USER -c 'id -nG'
+
+sudo chown root:docker /var/run/docker.sock
+
+git config --global user.email "chaitrasradha@gmail.com"
+git config --global user.name "Chaitraradha"
+git add .
+git commit -m "backup"
+git push  main
